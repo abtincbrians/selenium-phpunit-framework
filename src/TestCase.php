afff4706df9cb7388334697c2b8d3d8fead3d5f3
Brian Shirey <brian.shirey@atlanticbt.com>
2015-06-19 13:49:41 -0400

    Update to make Configurable a reusable trait, convert TestCase to Selenium2 test case.

--- src/TestCase.php
+++ src/TestCase.php
@@ -3,88 +3,11 @@ namespace SeleniumPhp;
 
 use SeleniumPhp\Config\ConfigFactory;
 use SeleniumPhp\Config\ConfigInterface;
-use PHPUnit_Extensions_SeleniumTestCase;
+use PHPUnit_Extensions_Selenium2TestCase;
+use SeleniumPhp\Configurable;
 use SeleniumPhp\Writer\Writer; // Leave this in place for dev purposes
 
-/**
- * Class TestCase
- *
- * 2015-05-01: Leaving this as an abstract class, though
- *              it no longer contains any abstract functions.
- *
- */
-abstract class TestCase extends PHPUnit_Extensions_SeleniumTestCase
+class TestCase extends PHPUnit_Extensions_Selenium2TestCase
 {
-    /**
-     * @var ConfigInterface
-     */
-    protected $config;
-
-    /**
-     * @var string
-     */
-    protected $configKey;
-
-    /**
-     * @param ConfigInterface $config
-     * @return mixed
-     */
-    public function setConfig(ConfigInterface $config)
-    {
-        $this->config = $config;
-
-        return this;
-    }
-
-    /**
-     * @return ConfigInterface
-     */
-    public function getConfig()
-    {
-        if (!isset($this->config)) {
-            $this->config =
-                ConfigFactory::getinstance()
-                    ->getConfiguration($this->getConfigKey());
-        }
-
-        return $this->config;
-    }
-
-    /**
-     * @param string $configKey
-     * @return $this
-     */
-    public function setConfigKey($configKey)
-    {
-        $this->configKey = $configKey;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getConfigKey()
-    {
-        if (!isset($this->configKey)) {
-            $this->configKey = $this->defineConfigkey();
-        }
-
-        return $this->configKey;
-    }
-
-    /**
-     * Use this in your child class to define a custom
-     * config key, otherwise your test config key is
-     * assumed to match your test's class name.
-     *
-     * Since class names for your tests *might* change (who knows)
-     * it may be better to override this function to return
-     * a specified key (ex: 'homepage')
-     *
-     * @return string
-     */
-    protected function defineConfigKey()
-    {
-        return get_called_class();
-    }
+    use Configurable;
 }
